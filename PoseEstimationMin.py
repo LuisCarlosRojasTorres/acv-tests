73d1635ccd6997cccbc4b419b4b2ca8d81fc927c
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2024-04-02 20:51:13 -0300

    Added angle

--- PoseEstimationMin.py
+++ PoseEstimationMin.py
@@ -1,12 +1,30 @@
 import cv2 
 import mediapipe as mp
 import time
+import numpy as np
+
+def calculate_angle(a,b,c):
+    a = np.array(a) # First
+    b = np.array(b) # Mid
+    c = np.array(c) # End
+    
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+    
+    if angle >180.0:
+        angle = 360-angle
+        
+    return angle 
 
 mpPose = mp.solutions.pose
 pose = mpPose.Pose()
 mpDraw = mp.solutions.drawing_utils
 
-cap = cv2.VideoCapture('./media/vid03b.mp4')
+cap = cv2.VideoCapture('./media/rosca_direta.mp4')
+#Getting resolution
+frame_width = int(cap.get(3))
+frame_height = int(0.9*cap.get(4)) 
+
 #cap = cv2.VideoCapture(0) #0 or 1 is for WEBCAM this is HW dependant
 previousTime = 0
 currentTime = 0
@@ -18,6 +36,21 @@ while True:
 
     if results.pose_landmarks:
         mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        landmarks = results.pose_landmarks.landmark       
+        
+        # Get coordinates
+        shoulder = [landmarks[mpPose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mpPose.PoseLandmark.LEFT_SHOULDER.value].y]
+        elbow = [landmarks[mpPose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mpPose.PoseLandmark.LEFT_ELBOW.value].y]
+        wrist = [landmarks[mpPose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mpPose.PoseLandmark.LEFT_WRIST.value].y]
+
+        # Calculate angle
+        angle = calculate_angle(shoulder, elbow, wrist)
+            
+            # Visualize angle
+        cv2.putText(img, "Angle: " + str(angle), 
+                    (10,frame_height), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                    )
 
     currentTime = time.time()
     fps = 1/(currentTime - previousTime)
