8ce4c18b34f5e43e00e5a481b368c4a79eb54985
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2024-04-09 08:59:06 -0300

    Tested, it works

--- Modules/VideoCapturer.py
+++ Modules/VideoCapturer.py
@@ -3,25 +3,50 @@ import mediapipe as mp
 import time
 import numpy as np
 
+class VideoCapturer:
+    capture = None
+    frame_width = 0
+    frame_height = 0
+    
+    def __init__(self,videoSource):
+        self.capture=cv2.VideoCapture(videoSource)
+        self.frame_width = int(self.capture.get(3))
+        self.frame_height = int(self.capture.get(4)) 
+
+    def getCapture(self):
+        return self.capture
+
+    def getWidth(self):
+        return self.frame_width
+    
+    def getHeight(self):
+        return self.frame_height
+    
+    def ReadCapture(self):
+        return self.capture.read()
+    
 mpPose = mp.solutions.pose
 pose = mpPose.Pose()
 mpDraw = mp.solutions.drawing_utils
 
 videoSource = './media/rosca_direta.mp4'
+vcap = VideoCapturer(videoSource)
 
-
-cap = cv2.VideoCapture(videoSource)
+##cap = cv2.VideoCapture(videoSource)
 #cap = cv2.VideoCapture(0) #0 or 1 is for WEBCAM this is HW dependant
 #Getting resolution
 
-frame_width = int(cap.get(3))
-frame_height = int(cap.get(4)) 
+##frame_width = int(cap.get(3))
+##frame_height = int(cap.get(4)) 
+
+frame_width = vcap.frame_width
+frame_height = vcap.frame_width 
 
 previousTime = 0
 currentTime = 0
 
 while True:
-    success, frame = cap.read()
+    success, frame = vcap.ReadCapture()
     frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = pose.process(frameRGB)
     
@@ -39,24 +64,7 @@ while True:
     cv2.waitKey(1)
 
 
-class VideoCapturer:
-    
-    def __init__(self):
-        self.capture=cv2.VideoCapture(videoSource)
-        self.frame_width = int(cap.get(3))
-        self.frame_height = int(cap.get(4)) 
 
-    def getCapture(self):
-        return self.capture
-
-    def getWidth(self):
-        return self.frame_width
-    
-    def getHeight(self):
-        return self.frame_height
-    
-    def ReadCapture(self):
-        return self.capture.read()
     
 #class Landmarker:
 
